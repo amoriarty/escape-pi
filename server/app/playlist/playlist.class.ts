a6a0d04d6ffd8b9ac4233c654300af061115ae62
Alexandre LEGENT <alexandrelegent@gmail.com>
2017-08-02 10:51:12 +0200

    Playlist are now saving in a mongodb database.

--- server/app/playlist/playlist.class.ts
+++ server/app/playlist/playlist.class.ts
@@ -1,18 +1,36 @@
+import * as mongodb from 'mongodb';
+
 import Socket from '../socket/socket.class';
 import { PlaylistInterface } from './playlist.interface';
 
 export default class Playlist extends Socket {
 	private callback: (name: String) => void;
 	private playlists: Playlist[] = [];
+	private MongoClient = new mongodb.MongoClient();
+	private db: mongodb.Db;
+	private collection: mongodb.Collection;
 
 	constructor(socket) {
 		super(socket);
+		this.MongoClient.connect(process.env.MONGODB_URL, (err, db) => {
+			if (process.env.NODE_ENV == "development" && err)
+				console.error(err);
+			if (process.env.NODE_ENV == "development")
+				console.log("mongodb connected");
+			this.db = db;
+			this.collection = db.collection('playlist');
+		});
 		this.socket.emit('playlist', this.playlists);
-		this.socket.on('playlist', (playlist: Playlist) => { this.savePlaylist(playlist); });
+		this.socket.on('playlist', (playlist: PlaylistInterface) => { this.savePlaylist(playlist); });
 	}
 
-	private savePlaylist(playlist: Playlist) {
-		console.log(playlist);
+	private savePlaylist(playlist: PlaylistInterface) {
+		this.collection.insertOne(playlist, (err, result) => {
+			if (process.env.NODE_ENV == "development") {
+				if (err) console.error(err);
+				else console.log("playlist saved:", playlist);
+			}
+		});
 	}
 
 	set read(callback) {
