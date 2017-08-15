2709909505f162ed15634b8b80f2221b5802a5cc
Alexandre LEGENT <alexandrelegent@gmail.com>
2017-08-15 18:28:52 +0200

    Playlist on angular client react well. Selection is much more easier from outside the component now.

--- server/public/src/app/selector/selector.component.ts
+++ server/public/src/app/selector/selector.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class SelectorComponent implements OnInit {
   @Input() list: String[];
   @Input() placeholder: String;
+  @Input() model: String;
   @Output() selected = new EventEmitter<String>();
 
   constructor() { }
